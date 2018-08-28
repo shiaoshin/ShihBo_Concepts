4e04c2f6bd4c67317edf4f343b2aa583af623bb6
shiaoshin <shiaoshinm@gmail.com>
2018-08-28 14:36:59 -0500

    Update images & Add Common functionality

--- qrcode.js
+++ qrcode.js
@@ -13,11 +13,24 @@ $(function(){
         $.each(chars,function(ind, alphabet){
             var validate = alphabet.match(/[A-Za-z]/);
             if(validate != null){
-                $("#qr_output").append("<img src='img/alphabets/"+alphabet+".jpg'>");
+                $("#qr_output").append("<img src='img/alphabets/"+alphabet+".png'>");
             }else{
                 var msg = (alphabet == " ")?"space":alphabet;
                 M.toast({html: msg+" is not supported!", displayLength: toastDuration});
             };
         })
+        
+        $("#qr_common").text("Add Common");
+    })
+    
+    /* Add Common */
+    $("#qr_common").on("click",function(){
+        if($(this).text() == "Add Common"){
+            $("#qr_output > img").addClass("common");
+            $(this).text("Remove Common");
+        }else{
+            $("#qr_output > img").removeClass("common");
+            $(this).text("Add Common");
+        }
     })
 })
